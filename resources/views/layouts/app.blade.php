d4c2784f87e9b87141e017838510eeeb8dc4460c
IanDizon <dznn20@gmail.com>
2025-08-20 08:05:41 +0800

    Update Tailwind CSS and related dependencies; configure purge paths in Tailwind config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title', 'Dashboard')</title>
 
     <!-- Tailwind CSS -->
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
